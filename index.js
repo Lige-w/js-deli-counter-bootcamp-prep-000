9483d3df5ac2272640e8327c4b0d5d472335ebbe
Lige <lige.williamson@gmail.com>
2019-01-31 21:05:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
-var katzDeli = [];
-
-function takeANumber(katzDeliLine, name) {
-  katzDeliLine.push(name)
-  katzDeli.push(`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
-
-  return katzDeli.pop()
+const takeANumber = (line, name) => {
+  line.push(name);
+  return `Welcome, ${name}. You are number ${line.length} in line.`;
 }
 
-function nowServing(katzDeliLine) {
-  if (katzDeliLine.length > 0){
-    return `Currently serving ${katzDeliLine.shift()}.`
+const nowServing = line => {
+  if (!line.length) {
+    return "There is nobody waiting to be served!";
   } else {
-    return "There is nobody waiting to be served!"
+    return `Currently serving ${line.shift()}.`;
   }
 }
 
-var line = []
-function currentLine(katzDeliLine) {
-  let i= 0
-  while (i < katzDeliLine.length) {
-    line.push(` `+[i+1]+`. `  + katzDeliLine[i])
-    i++;
-  }
-  if (katzDeliLine.length > 0) {
-    return `The line is currently:${line}`
-  } else{
-    return "The line is currently empty."
+const currentLine = line => {
+  if (!line.length) {
+    return "The line is currently empty.";
+  } else {
+    let numberedLine = [];
+    for (let i = 0; i < line.length; i++) {
+      numberedLine.push(`${i + 1}. ${line[i]}`)
+    }
+    return `The line is currently: ${numberedLine.join(', ')}`;
   }
 }
